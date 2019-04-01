a96cc872b090ca2c9abaa2bc573b89f629e60324
rails <kanha.dubey9@gmail.com>
2019-04-01 11:23:33 +0530

    adding precompile false

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module Shopping
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.action_cable.url = "/cable"  
+    config.action_cable.mount_path = '/websocket'
+    config.action_cable.allowed_request_origins = ['http://localhost:3000']
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
